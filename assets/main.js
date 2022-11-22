03982a10dace258f7debbda155469ebd8ee5ad9a
Yara <yara.wagner@stud.fhgr.ch>
2022-11-22 10:52:19 +0100

    Ordnerstruktur aufräumen

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+// Spielfeld initialisieren
+
+// Startspieler bestimmen
+// Startspieler mitteilen
+
+// Zug ausführen
+    // Würfeln
+        // erneut Würfeln
+    // Spielfigur bewegen
+    // Feld auf Leiter prüfen
+        // Leiter verwenden
+
+// Spielstand prüfen
+    //Sieg feststellen / mitteilen
+
+// Spiel beenden / zurücksetzen
