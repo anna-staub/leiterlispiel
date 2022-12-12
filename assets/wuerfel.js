a45514e92a058f2a67df0c9beb8e1cbdbb140509
Anna Staub <info@anna-staub.ch>
2022-12-12 11:37:57 +0100

    Würfel auskommentiert

--- assets/wuerfel.js
+++ assets/wuerfel.js
@@ -1,4 +1,5 @@
 // Würfel als eigenes Objekt
+
 // Ich bin nicht sicher, ob die Würfel-Funktion direkt dem Spieler als Methode gegeben werden sollte?
 // Ich glaube auf jeden Fall, wir sollten nicht alle Funktionalitäten in ein Objekt Spielfeld packen.
 
@@ -7,11 +8,17 @@ class Wuerfel{
     constructor(augenzahl) {
         this.Augenzahl = augenzahl;
     }
+    
+    // Würfel-Methode
     wuerfeln() {
         let wuerfelergebnis = Math.ceil(Math.random()*this.Augenzahl);
+
+        // Falls das Würfelergebnis 1-5 ist
         if (wuerfelergebnis < 6) {
             return wuerfelergebnis;
         }
+
+        // Falls das Würfelergebnis 6 ist
         else if (wuerfelergebnis === 6) {
             console.log(wuerfelergebnis)
             console.log("Sie dürfen noch einmal!")
@@ -20,4 +27,5 @@ class Wuerfel{
     }
   };
   
+  // Spielwürfel-Instanz erstellen
   spielwuerfel = new Wuerfel(6);
