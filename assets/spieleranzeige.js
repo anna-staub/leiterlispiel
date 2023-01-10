1c8ef78a0580ce91f52f150ee99340907ca7c66c
Yara <yara.wagner@stud.fhgr.ch>
2023-01-10 17:54:38 +0100

    Spieleranzeige in separatem File

--- assets/spieleranzeige.js
+++ assets/spieleranzeige.js
@@ -1,3 +1,14 @@
 class Spieleranzeige {
+    // DOM-Element für Spieleranzeige
+    domElementSpieleranzeige = document.createElement('div');
     
+    // Spieler in Spieleranzeige anzeigen
+    spielerAnzeigen(){
+        console.log('Aktueller Spieler: '+this.domElementSpieleranzeige.id);
+        document.getElementById('spieleranzeige').appendChild(this.domElementSpieleranzeige);
+    }
+    // Spieler aus Spieleranzeige entfernen
+    spielerAusAnzeigeEntfernen() {
+        document.getElementById('spieleranzeige').removeChild(this.domElementSpieleranzeige);
+    }
 }
