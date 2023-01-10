c1ecdf7aef9c4795307da0d26d95caffc1285dd3
Yara Wagner <yara.wagner@stud.fhgr.ch>
2023-01-10 17:06:08 +0000

    Revert "Spieleranzeige in separatem File"
    This reverts commit 1c8ef78a0580ce91f52f150ee99340907ca7c66c

--- assets/spieleranzeige.js
+++ assets/spieleranzeige.js
@@ -1,14 +1,3 @@
 class Spieleranzeige {
-    // DOM-Element für Spieleranzeige
-    domElementSpieleranzeige = document.createElement('div');
     
-    // Spieler in Spieleranzeige anzeigen
-    spielerAnzeigen(){
-        console.log('Aktueller Spieler: '+this.domElementSpieleranzeige.id);
-        document.getElementById('spieleranzeige').appendChild(this.domElementSpieleranzeige);
-    }
-    // Spieler aus Spieleranzeige entfernen
-    spielerAusAnzeigeEntfernen() {
-        document.getElementById('spieleranzeige').removeChild(this.domElementSpieleranzeige);
-    }
 }
