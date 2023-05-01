5e9e34f0a238a9f25ed25400e507e7dbeb9a68e7
Yara <yara.wagner@stud.fhgr.ch>
2023-05-01 16:57:11 +0200

    Korrektur: LocalStorage anstatt SessionStorage

--- assets/startseite.js
+++ assets/startseite.js
@@ -1,22 +1,22 @@
-// Wert aus Namensfeld1 in SessionStorage speichern
+// Wert aus Namensfeld1 in LocalStorage speichern
 let name1 = document.getElementById('name1');
 name1.addEventListener('change', () => {
     StorageService.set('name1', name1.value);
 });
 
-// Wert aus Namensfeld2 in SessionStorage speichern
+// Wert aus Namensfeld2 in LocalStorage speichern
 let name2 = document.getElementById('name2');
 name2.addEventListener('change', () => {
     StorageService.set('name2', name2.value);
 });
 
-// Wert aus Selektor1 in SessionStorage speichern
+// Wert aus Selektor1 in LocalStorage speichern
 let selektor1 = document.getElementById('selektor1');
 selektor1.addEventListener('change', () => {
     StorageService.set('farbe1', selektor1.value);
 });
 
-// Wert aus Selektor2 in SessionStorage speichern
+// Wert aus Selektor2 in LocalStorage speichern
 let selektor2 = document.getElementById('selektor2');
 selektor2.addEventListener('change', () => {
     StorageService.set('farbe2', selektor2.value);
