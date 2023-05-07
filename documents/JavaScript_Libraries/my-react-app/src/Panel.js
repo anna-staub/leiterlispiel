8c0386a3e24351e28ab24589d7febe6a33537796
Yara <yara.wagner@stud.fhgr.ch>
2023-05-07 11:23:39 +0200

    Korrektur Schreibfehler in Panel.js

--- documents/JavaScript_Libraries/my-react-app/src/Panel.js
+++ documents/JavaScript_Libraries/my-react-app/src/Panel.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 // Komponente Panel exportieren
 export default function Panel({ children }) {
-    const [open, setOpen] = useState(false); // open = Variable, setOpen = damit kann der Wert der Variable geämdert werden, useState = vordefinierter Hook, false = Default-Wert der Variable 'open'
+    const [open, setOpen] = useState(false); // open = Variable, setOpen = damit kann der Wert der Variable geändert werden, useState = vordefinierter Hook, false = Default-Wert der Variable 'open'
     // Inhalt der Komponente Panel
     // Wenn der Button angeklickt wird, wird die Variable 'open' auf den gegenteiligen Wert (true bzw. false) gesetzt.
     // Wenn die Variable 'open' den Wert true hat, wird im Button der Text 'Beschreibung ausblenden' angezeigt, bei false 'Beschreibung anzeigen'.
